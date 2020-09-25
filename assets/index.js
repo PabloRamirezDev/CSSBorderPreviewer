dca603ee5cb4c1c17b159d98797d9a786cf7541d
Pablo Ramírez <66808827+PabloSRC@users.noreply.github.com>
2020-09-25 13:22:17 -0400

    Added html, css and js main files

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#top-left-radius").change(function(){
+    let radius = $(this).val() + "px";
+    $(".preview-box").css("border-top-left-radius", radius);
+  });
+  $("#top-right-radius").change(function(){
+    let radius = $(this).val() + "px";
+    $(".preview-box").css("border-top-right-radius", radius);
+  });
+  $("#bottom-left-radius").change(function(){
+    let radius = $(this).val() + "px";
+    $(".preview-box").css("border-bottom-left-radius", radius);
+  });
+  $("#bottom-right-radius").change(function(){
+    let radius = $(this).val() + "px";
+    $(".preview-box").css("border-bottom-right-radius", radius);
+  });
+});
